cd2b7471e0af44a2c3147ec162977e9c623d6073
bibabibu369 <fedorra.2004@gmail.com>
2024-06-12 02:02:25 +0700

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,252 @@
+﻿namespace QL_DIEM
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.cbID = new System.Windows.Forms.ComboBox();
+            this.cbMH = new System.Windows.Forms.ComboBox();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.btnEdit = new System.Windows.Forms.Button();
+            this.btnDel = new System.Windows.Forms.Button();
+            this.dgvDiem = new System.Windows.Forms.DataGridView();
+            this.tbDiem = new System.Windows.Forms.TextBox();
+            this.tbTen = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDiem)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(73, 55);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Mã SV";
+            // 
+            // label2
+            // 
+            this.label2.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(73, 169);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(70, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Môn Học";
+            // 
+            // label3
+            // 
+            this.label3.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(73, 226);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(45, 20);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Điểm";
+            // 
+            // cbID
+            // 
+            this.cbID.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.cbID.FormattingEnabled = true;
+            this.cbID.Items.AddRange(new object[] {
+            "2251162125",
+            "2251162126",
+            "2251162127",
+            "2251162128",
+            "2251162129"});
+            this.cbID.Location = new System.Drawing.Point(191, 52);
+            this.cbID.Margin = new System.Windows.Forms.Padding(4);
+            this.cbID.Name = "cbID";
+            this.cbID.Size = new System.Drawing.Size(194, 28);
+            this.cbID.TabIndex = 3;
+            // 
+            // cbMH
+            // 
+            this.cbMH.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.cbMH.FormattingEnabled = true;
+            this.cbMH.Items.AddRange(new object[] {
+            "Van",
+            "Toan",
+            "Anh"});
+            this.cbMH.Location = new System.Drawing.Point(191, 167);
+            this.cbMH.Margin = new System.Windows.Forms.Padding(4);
+            this.cbMH.Name = "cbMH";
+            this.cbMH.Size = new System.Drawing.Size(194, 28);
+            this.cbMH.TabIndex = 4;
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.btnUpdate.Location = new System.Drawing.Point(545, 47);
+            this.btnUpdate.Margin = new System.Windows.Forms.Padding(4);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(126, 33);
+            this.btnUpdate.TabIndex = 6;
+            this.btnUpdate.Text = "Cập nhật điểm";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // btnReset
+            // 
+            this.btnReset.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.btnReset.Location = new System.Drawing.Point(545, 105);
+            this.btnReset.Margin = new System.Windows.Forms.Padding(4);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(126, 33);
+            this.btnReset.TabIndex = 7;
+            this.btnReset.Text = "Reset";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // btnEdit
+            // 
+            this.btnEdit.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.btnEdit.Location = new System.Drawing.Point(545, 163);
+            this.btnEdit.Margin = new System.Windows.Forms.Padding(4);
+            this.btnEdit.Name = "btnEdit";
+            this.btnEdit.Size = new System.Drawing.Size(126, 33);
+            this.btnEdit.TabIndex = 8;
+            this.btnEdit.Text = "Sửa";
+            this.btnEdit.UseVisualStyleBackColor = true;
+            this.btnEdit.Click += new System.EventHandler(this.btnEdit_Click);
+            // 
+            // btnDel
+            // 
+            this.btnDel.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.btnDel.Location = new System.Drawing.Point(545, 221);
+            this.btnDel.Margin = new System.Windows.Forms.Padding(4);
+            this.btnDel.Name = "btnDel";
+            this.btnDel.Size = new System.Drawing.Size(126, 33);
+            this.btnDel.TabIndex = 9;
+            this.btnDel.Text = "Xóa";
+            this.btnDel.UseVisualStyleBackColor = true;
+            this.btnDel.Click += new System.EventHandler(this.btnDel_Click);
+            // 
+            // dgvDiem
+            // 
+            this.dgvDiem.AllowUserToAddRows = false;
+            this.dgvDiem.AllowUserToDeleteRows = false;
+            this.dgvDiem.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dgvDiem.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgvDiem.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvDiem.Location = new System.Drawing.Point(77, 294);
+            this.dgvDiem.Margin = new System.Windows.Forms.Padding(4);
+            this.dgvDiem.Name = "dgvDiem";
+            this.dgvDiem.ReadOnly = true;
+            this.dgvDiem.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dgvDiem.Size = new System.Drawing.Size(707, 207);
+            this.dgvDiem.TabIndex = 10;
+            this.dgvDiem.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvDiem_CellContentClick);
+            this.dgvDiem.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvDiem_CellContentClick);
+            // 
+            // tbDiem
+            // 
+            this.tbDiem.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.tbDiem.Location = new System.Drawing.Point(191, 225);
+            this.tbDiem.Name = "tbDiem";
+            this.tbDiem.Size = new System.Drawing.Size(194, 27);
+            this.tbDiem.TabIndex = 11;
+            // 
+            // tbTen
+            // 
+            this.tbTen.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.tbTen.Location = new System.Drawing.Point(191, 110);
+            this.tbTen.Name = "tbTen";
+            this.tbTen.Size = new System.Drawing.Size(194, 27);
+            this.tbTen.TabIndex = 13;
+            // 
+            // label4
+            // 
+            this.label4.AllowDrop = true;
+            this.label4.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(73, 112);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(56, 20);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "Họ Tên";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(867, 545);
+            this.Controls.Add(this.tbTen);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.tbDiem);
+            this.Controls.Add(this.dgvDiem);
+            this.Controls.Add(this.btnDel);
+            this.Controls.Add(this.btnEdit);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.cbMH);
+            this.Controls.Add(this.cbID);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "ĐẠI HỌC THỦY LỢI";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDiem)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox cbID;
+        private System.Windows.Forms.ComboBox cbMH;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.Button btnEdit;
+        private System.Windows.Forms.Button btnDel;
+        private System.Windows.Forms.DataGridView dgvDiem;
+        private System.Windows.Forms.TextBox tbDiem;
+        private System.Windows.Forms.TextBox tbTen;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,202 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QL_DIEM
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // Tao chuoi ket noi va cac bien
+        string ketnoi = "Data Source=.\\sqlexpress;Initial Catalog=QL_DIEM;Integrated Security=True;TrustServerCertificate=True";
+        SqlConnection conn;
+        SqlCommand cmd;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Load du lieu tu database len datagridview
+            conn = new SqlConnection(ketnoi);
+            conn.Open();
+
+            string sql = "SELECT * FROM DIEM";
+            SqlDataAdapter daDiem = new SqlDataAdapter(sql, conn);
+            DataTable dtDiem = new DataTable();
+            daDiem.Fill(dtDiem);
+            dgvDiem.DataSource = dtDiem;
+        }
+
+        // Xoa du lieu trong control va focus vao ma sv
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            Clear(cbID);
+            Clear(cbMH);
+            tbDiem.Clear();
+            tbTen.Clear();
+            cbID.Focus();
+        }
+
+        // Tao phuong thuc clear cho combobox
+        public void Clear(ComboBox e)
+        {
+            e.SelectedIndex = -1;
+            e.Text = string.Empty;
+        }
+
+        private void dgvDiem_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex >= 0)
+            {
+                DataGridViewRow row = dgvDiem.Rows[e.RowIndex];
+
+                cbID.Text = row.Cells["Mã SV"].Value.ToString().Trim();
+                tbTen.Text = row.Cells["Họ Tên"].Value.ToString().Trim();
+                cbMH.Text = row.Cells["Môn Học"].Value.ToString().Trim();
+                tbDiem.Text = row.Cells["Điểm"].Value.ToString().Trim();
+            }
+        }
+
+        // Phuong thuc reload csdl
+        public void rf()
+        {
+            using (conn = new SqlConnection(ketnoi))
+            {
+                conn.Open();
+
+                string sql = "SELECT * FROM DIEM";
+                SqlDataAdapter daDiem = new SqlDataAdapter(sql, conn);
+                DataTable dtDiem = new DataTable();
+                daDiem.Fill(dtDiem);
+                dgvDiem.DataSource = dtDiem;
+            }
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            // tao cac bien tuong ung
+            string maSv = cbID.Text.Trim();
+            string tenSv = tbTen.Text.Trim();
+            string monHoc = cbMH.Text.Trim();
+            string diem = tbDiem.Text.Trim();
+
+            try
+            {
+                using (conn = new SqlConnection(ketnoi))
+                {
+                    conn.Open();
+                    string add = "INSERT INTO DIEM ([Mã SV], [Họ Tên], [Môn Học], Điểm) VALUES (@maSv, @tenSv, @monHoc, @diem)";
+
+                    using (cmd = new SqlCommand(add, conn))
+                    {
+                        cmd.Parameters.AddWithValue("@maSv", maSv);
+                        cmd.Parameters.AddWithValue("@tenSv", tenSv);
+                        cmd.Parameters.AddWithValue("@monHoc", monHoc);
+                        cmd.Parameters.AddWithValue("@diem", diem);
+
+                        if (cmd.ExecuteNonQuery() > 0)
+                        {
+                            MessageBox.Show("Them thanh cong!", "Thong bao", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                            rf();
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Da xay ra loi: " + ex.Message, "Thong bao", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                throw;
+            }
+        }
+
+        private void btnDel_Click(object sender, EventArgs e)
+        {
+            // tao cac bien tuong ung
+            string maSv = cbID.Text.Trim();
+            string tenSv = tbTen.Text.Trim();
+            string monHoc = cbMH.Text.Trim();
+            string diem = tbDiem.Text.Trim();
+
+            try
+            {
+                using (conn = new SqlConnection(ketnoi))
+                {
+                    conn.Open();
+                    string del = "DELETE FROM DIEM WHERE [Mã SV] = @maSv";
+
+                    using (cmd = new SqlCommand(del, conn))
+                    {
+                        cmd.Parameters.AddWithValue("@maSv", maSv);
+
+                        if (cmd.ExecuteNonQuery() > 0)
+                        {
+                            MessageBox.Show("Xoa thanh cong!", "Thong bao", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                            rf();
+                            Clear(cbID);
+                            Clear(cbMH);
+                            tbDiem.Clear();
+                            tbTen.Clear();
+                            cbID.Focus();
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Da xay ra loi: " + ex.Message, "Thong bao", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                throw;
+            }
+        }
+
+        private void btnEdit_Click(object sender, EventArgs e)
+        {
+            // tao cac bien tuong ung
+            string maSv = cbID.Text.Trim();
+            string tenSv = tbTen.Text.Trim();
+            string monHoc = cbMH.Text.Trim();
+            string diem = tbDiem.Text.Trim();
+
+            try
+            {
+                using (conn = new SqlConnection(ketnoi))
+                {
+                    conn.Open();
+                    string del = "UPDATE DIEM SET [Mã SV] = @maSv, [Họ Tên] = @tenSv, [Môn Học] = @monHoc, Điểm = @diem";
+
+                    using (cmd = new SqlCommand(del, conn))
+                    {
+                        cmd.Parameters.AddWithValue("@maSv", maSv);
+                        cmd.Parameters.AddWithValue("@tenSv", tenSv);
+                        cmd.Parameters.AddWithValue("@monHoc", monHoc);
+                        cmd.Parameters.AddWithValue("@diem", diem);
+
+                        if (cmd.ExecuteNonQuery() > 0)
+                        {
+                            MessageBox.Show("Sua thanh cong!", "Thong bao", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                            rf();
+                            Clear(cbID);
+                            Clear(cbMH);
+                            tbDiem.Clear();
+                            tbTen.Clear();
+                            cbID.Focus();
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Da xay ra loi: " + ex.Message, "Thong bao", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                throw;
+            }
+        }
+    }
+}
